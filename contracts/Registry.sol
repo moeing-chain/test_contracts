3d8778b9074ba8ab11e57900f0d4c52b1cfccd49
wangkui0508 <wangkui0508@hotmail.com>
2021-01-28 14:47:58 +0800

    Change Registry's variable name

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -5,66 +5,67 @@ import "./interfaces/IRegistry.sol";
 
 contract Registry is IRegistry {
 
-    mapping(address => bytes32) private accountTable;
-    mapping(bytes32 => address) private ownerTable;
-    mapping(bytes32 => address) private operatorTable;
-    mapping(bytes32 => address) private newOperatorTable;
-    mapping(address /*owner*/ => address) private allowanceTable;
+    mapping(address => bytes32) private ownerToName;
+    mapping(bytes32 => address) private nameToOwner;
+    mapping(bytes32 => address) private nameToOperator;
+    mapping(bytes32 => address) private nameToNewOperator;
+    mapping(address /*owner*/ => address) private ownerToApprovedContract;
     mapping(address => address) public spaceTable;
 
     function Registry(){}
 
     function register(bytes32 accountName, address operator) external returns (bool) {
-        if (ownerTable[msg.sender] == address(0)) {
-            bytes32 memory oldAccount = accountTable[msg.sender];
-            accountTable[msg.sender] = accountName;
-            delete ownerTable[oldAccount];
-            delete operatorTable[oldAccount];
-            delete newOperatorTable[oldAccount];
-            ownerTable[accountName] = msg.sender;
-            operatorTable[accountName] = operator;
+        if (nameToOwner[accountName] == address(0)) {
+            bytes32 memory oldAccount = ownerToName[msg.sender];
+            ownerToName[msg.sender] = accountName;
+            delete nameToOwner[oldAccount];
+            delete nameToOperator[oldAccount];
+            delete nameToNewOperator[oldAccount];
+            nameToOwner[accountName] = msg.sender;
+            nameToOperator[accountName] = operator;
             emit Register(accountName, msg.sender, operator);
             return true;
         }
         return false;
     }
 
+    // Maybe we should emit an event for this?
     function appointNewOperator(bytes32 accountName, address operator) external {
-        require(ownerTable[accountName] == msg.sender);
-        newOperatorTable[accountName] = operator;
+        require(nameToOwner[accountName] == msg.sender);
+        nameToNewOperator[accountName] = operator;
     }
 
     // Switch to a newly-appointed operator. Only the newly-appointed operator can call this function.
     function switchToNewOperator(bytes32 accountName) external {
-        require(newOperatorTable[accountName] == msg.sender);
-        operatorTable[accountName] = msg.sender;
-        delete newOperatorTable[accountName];
+        require(nameToNewOperator[accountName] == msg.sender);
+        nameToOperator[accountName] = msg.sender;
+        delete nameToNewOperator[accountName];
         emit ChangeOperator(accountName, msg.sender);
     }
 
     // Approve a smart contract to transfer name's ownership
     //todo: like erc721, ownership can only approve one address the same time
     function approve(address contractAddr) external {
-        allowanceTable[msg.sender] = contractAddr;
+        ownerToApprovedContract[msg.sender] = contractAddr;
     }
 
     // Transfer accountName's ownership from oldOwner to newOwner
     //todo: only owner can transfer, oldOwner not need
     function transfer(bytes32 accountName, address oldOwner, address newOwner) external {
-        require(ownerTable[accountName] == msg.sender);
-        accountTable[newOwner] = accountName;
-        delete accountTable[msg.sender];
-        ownerTable[accountName] = newOwner;
-        address tmp = allowanceTable[msg.sender];
+        require(nameToOwner[accountName] == msg.sender);
+        ownerToName[newOwner] = accountName;
+        delete ownerToName[msg.sender];
+        nameToOwner[accountName] = newOwner;
+        address tmp = ownerToApprovedContract[msg.sender];
         if (tmp != address(0)) {
-            delete allowanceTable[msg.sender];
-            allowanceTable[newOwner] = tmp;
+            delete ownerToApprovedContract[msg.sender];
+            ownerToApprovedContract[newOwner] = tmp;
         }
     }
 
     // Given the owner's address, query the corresponding accountName
     function getAccountNameByOwner(address owner) external view returns (bytes32) {
-        return accountTable[owner];
+        return ownerToName[owner];
     }
 
     // Given the operator's address, query the corresponding accountName
@@ -75,20 +76,20 @@ contract Registry is IRegistry {
 
     // Given the accountName, query the addresses of the owner and operator
     function getOwnerAndOperatorByAccountName(bytes32 accountName) external view returns (address, address) {
-        address owner = ownerTable[accountName];
-        address operator = operatorTable[accountName];
+        address owner = nameToOwner[accountName];
+        address operator = nameToOperator[accountName];
         return (owner, operator);
     }
 
     // Given the accountName, query the newly-appointed operator
     function getNewOperator(bytes32 accountName) external view returns (address) {
-        return newOperatorTable[accountName];
+        return nameToNewOperator[accountName];
     }
 
     //todo: add this
     function getOperatorByOwner(address owner) external view returns (address) {
-        bytes32 accName = accountTable[owner];
-        return operatorTable[accName];
+        bytes32 accName = ownerToName[owner];
+        return nameToOperator[accName];
     }
 
     //todo: add this
